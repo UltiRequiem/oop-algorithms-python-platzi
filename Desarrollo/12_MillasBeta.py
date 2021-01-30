8e7b4704f1d51f83b5f329922148db193019d2f2
Zer <eliaz.bobadilladev@gmail.com>
2021-01-30 12:55:12 -0500

    Hacking The Future

---
+++ Desarrollo/12_MillasBeta.py
@@ -0,0 +1,19 @@
+class Millas:
+	def __init__(self, distancia = 0):
+		self.distancia = distancia
+
+	def convertir_a_kilometros(self):
+		return (self.distancia * 1.609344)
+
+	# Método getter
+	def obtener_distancia(self):
+		return self._distancia
+
+	# Método setter
+	def definir_distancia(self, valor):
+		if valor < 0:
+			raise ValueError("No es posible convertir distancias menores a 0.")
+		self._distancia = valor
+
+
+'''Si probamos nuestro código funcionará, la desventaja es que cualquier aplicación que hayamos creado con una base similar deberá ser actualizado. Esto no es nada escalable si tenemos cientos o miles de líneas de código.'''
