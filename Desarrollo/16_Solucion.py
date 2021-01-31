7414dd31634570015a883ccff06784e654dc2734
Zer <eliaz.bobadilladev@gmail.com>
2021-01-31 10:56:32 -0500

    dormir

--- Desarrollo/16_Solucion.py
+++ Desarrollo/16_Solucion.py
@@ -1,12 +1,15 @@
 class Perro():
-    def __init__(self, nombre, color,raza):
-      self.nombre = nombre
-      self.color = color
-      self.raza = raza
-      self.comida = 10
+    def __init__(self, nombre, color, raza):
+        self.nombre = nombre
+        self.color = color
+        self.raza = raza
+        self.comida = 10
+
+    def dormir(self):
+        return 'zZZZzzzZZZzzzZZZ'
 
     def ladrar(self):
-        return'El perro dice wuao wuao'
+        return 'El perro dice wuao wuao'
 
     def comer(self, cantidad):
         self.comida = self.comida - cantidad
@@ -15,11 +18,11 @@ class Perro():
 
 if __name__ == '__main__':
     perro = Perro('Lucas', 'Negro', 'Bulldog')
-    
+
     croquetas = int(input('¿Cuantas croquetas va a comer el perro? \n'))
-    
+
     if croquetas < 10:
-        print('El perro comio '+str(croquetas)+' croquetas.'+'Ahora al Perro le quedan '+str(perro.comer(croquetas))+' croquetas.')
+        print('El perro comio '+str(croquetas)+' croquetas.' +'Ahora al Perro le quedan '+str(perro.comer(croquetas))+' croquetas.')
     else:
         print('¡El perro No puede comer tantas Croquetas!')
 
