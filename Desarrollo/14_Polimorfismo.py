f733437d54168c47519a1bb62341fbd45865af9f
Zer <eliaz.bobadilladev@gmail.com>
2021-01-30 21:29:07 -0500

    Polimorfismo

---
+++ Desarrollo/14_Polimorfismo.py
@@ -0,0 +1,28 @@
+class Persona:
+
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def avanza(self):
+        print('Ando caminando')
+
+
+class Ciclista(Persona):
+
+    def __init__(self, nombre):
+        super().__init__(nombre)
+
+    def avanza(self):
+        print('Ando moviendome en mi bicicleta')
+
+
+def main():
+    persona = Persona('David')
+    persona.avanza()
+
+    ciclista = Ciclista('Daniel')
+    ciclista.avanza()
+
+
+if __name__ == '__main__':
+    main()
