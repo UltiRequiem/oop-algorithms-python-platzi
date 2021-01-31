0007301bfeb2aae7cd708193a8a29d4d252b500d
Zer <eliaz.bobadilladev@gmail.com>
2021-01-31 10:51:49 -0500

    Solucion pro

---
+++ Desarrollo/16_Solucion.py
@@ -0,0 +1,26 @@
+class Perro():
+    def __init__(self, nombre, color,raza):
+      self.nombre = nombre
+      self.color = color
+      self.raza = raza
+      self.comida = 10
+
+    def ladrar(self):
+        return'El perro dice wuao wuao'
+
+    def comer(self, cantidad):
+        self.comida = self.comida - cantidad
+        return self.comida
+
+
+if __name__ == '__main__':
+    perro = Perro('Lucas', 'Negro', 'Bulldog')
+    
+    croquetas = int(input('¿Cuantas croquetas va a comer el perro? \n'))
+    
+    if croquetas < 10:
+        print('El perro comio '+str(croquetas)+' croquetas.'+'Ahora al Perro le quedan '+str(perro.comer(croquetas))+' croquetas.')
+    else:
+        print('¡El perro No puede comer tantas Croquetas!')
+
+# https://platzi.com/comentario/2224673/
