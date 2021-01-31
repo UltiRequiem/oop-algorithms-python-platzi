843f8f50776ca3c630c745274337bc6a62633036
Zer <eliaz.bobadilladev@gmail.com>
2021-01-31 12:51:42 -0500

    La Busqueda Lineal

---
+++ Desarrollo/20_BusquedaLineal.py
@@ -0,0 +1,22 @@
+import random
+
+def busqueda_lineal(lista, objetivo):
+    match = False
+
+    for elemento in lista: # O(n)
+        if elemento == objetivo:
+            match = True
+            break
+
+    return match
+
+
+if __name__ == '__main__':
+    tamano_de_lista = int(input('De que tamano sera la lista? '))
+    objetivo = int(input('Que numero quieres encontrar? '))
+
+    lista = [random.randint(0, 100) for i in range(tamano_de_lista)]
+
+    encontrado = busqueda_lineal(lista, objetivo)
+    print(lista)
+    print(f'El elemento {objetivo} {"esta" if encontrado else "no esta"} en la lista')
